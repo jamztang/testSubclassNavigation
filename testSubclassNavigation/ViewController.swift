eb4931d12dfbb752eba8ac913bfe3e6ead096ca2
James Tang <james@goodnotesapp.com>
2020-09-29 11:32:34 +0800

    Initial Commit

---
+++ testSubclassNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testSubclassNavigation
+//
+//  Created by James Tang on 29/9/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
